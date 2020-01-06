6a120122154299a546bf69c8052c368767013b7d
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2020-01-06 16:30:54 -0500

    Version 0.3.0

--- extensionSettingsUpdater.js
+++ extensionSettingsUpdater.js
@@ -1,5 +1,7 @@
 let darkModeCheckbox = document.querySelector("#container > div:nth-child(3) > label > input[type=checkbox]");
 let gpaCalcCheckbox = document.querySelector("#container > div:nth-child(4) > label > input[type=checkbox]");
+let hypoGradeCheckbox = document.querySelector("#container > div:nth-child(5) > label > input[type=checkbox]");
+
 chrome.storage.local.get(["psDarkMode"], function(items){
     if(items["psDarkMode"]) {
         darkModeCheckbox.checked = true;
@@ -12,12 +14,23 @@ chrome.storage.local.get(["psGPACalc"], function(items){
     }
 });
 
+
+chrome.storage.local.get(["psHypoGradeCalc"], function(items){
+    if(items["psHypoGradeCalc"]) {
+        hypoGradeCheckbox.checked = true;
+    }
+});
+
 darkModeCheckbox.onclick = function() {
     updateDarkModeSetting();
 }
 
 gpaCalcCheckbox.onclick = function() {
     updateGPACalcSetting();
+}
+
+hypoGradeCheckbox.onclick = function() {
+    updateHypoGradeSetting();
 }
 
 function updateDarkModeSetting() {
@@ -33,5 +46,13 @@ function updateGPACalcSetting() {
         chrome.storage.local.set({ "psGPACalc": true }, function(){});
     } else {
         chrome.storage.local.set({ "psGPACalc": false }, function(){});
+    }
+}
+
+function updateHypoGradeSetting() {
+    if(hypoGradeCheckbox.checked) {
+        chrome.storage.local.set({ "psHypoGradeCalc": true }, function(){});
+    } else {
+        chrome.storage.local.set({ "psHypoGradeCalc": false }, function(){});
     }
 }
---
+++ gradeEditCalculator.js
@@ -0,0 +1,70 @@
+chrome.storage.local.get(["psHypoGradeCalc"], function(items){
+    if(items["psHypoGradeCalc"]) {
+        let gradeEdited = false;
+        let firstRun = true;
+        
+        function calculateGrade() {
+            const Grade = class {
+                constructor(pointsEarned, pointsPossible) {
+                    this.ptE = pointsEarned;
+                    this.ptP = pointsPossible;
+                }
+            }
+            
+            let grades = [];
+            let gradesTable = document.querySelector("#scoreTable");
+            for(let i = 1; i < gradesTable.rows.length - 1; i++) {
+                let element = gradesTable.rows[i].cells[10].querySelector("span");
+                let gradeSplit = [];
+                if(element.innerHTML.includes("input")) {
+                    gradeSplit.push(element.querySelector("input[type=text]:nth-child(1)").value);
+                    gradeSplit.push(element.querySelector("input[type=text]:nth-child(2)").value);
+                } else {
+                    gradeSplit = element.innerHTML.trim().split("/");
+                }
+                if(firstRun) {
+                element.ondblclick = function() {
+                    let newElement = changeElementToInput(element);
+                    newElement.ondblclick = null;
+                }
+            }
+                
+                element.style.cursor = "pointer";
+                if(gradeSplit[0] !== "--") {
+                    let newGrade = new Grade(parseFloat(gradeSplit[0]), parseFloat(gradeSplit[1]));
+                    grades.push(newGrade);
+                }
+            }
+            
+            let gradePtE = 0;
+            let gradePtP = 0;
+            
+            for(let i = 0; i < grades.length; i++) {
+                gradePtE += grades[i].ptE;
+                gradePtP += grades[i].ptP;
+            }
+            
+            firstRun = false;
+            return Math.round((gradePtE / gradePtP) * 10000) / 100;
+        }
+        
+        function changeElementToInput(element) {
+            let currentScore = element.innerHTML.trim();
+            let splitScore = currentScore.split("/");
+            element.outerHTML = "<span style=\"display: flex;\"><input size='3' type='text' value='" + splitScore[0] + "'>/<input size='3' type='text' value='" + splitScore[1] + "'>";
+            gradeEdited = true;
+            return element;
+        }
+        
+        function changeElementToSpan(element) {
+            let currentScore = element.value;
+            element.outerHTML = '<span data-ng-if=\"studentAssignment._assignmentsections[0]._assignmentscores[0].scorepoints != null &amp;&amp; (studentAssignment._assignmentsections[0]._assignmentscores[0].scorepoints % 1) == 0 &amp;&amp; studentAssignment._assignmentsections[0].scoretype !== \'COLLECTED\'\" class=\"ng-binding ng-scope\"> ' + currentScore + ' </span>';
+        }
+        
+        setInterval(function() {
+            if(document.hasFocus()) {
+                document.querySelector("#classDetail > table > tbody > tr:nth-child(2) > td:nth-child(4)").innerHTML = calculateGrade() + "%" + (gradeEdited ? " (Edited)" : "");
+            }
+        }, 250);
+    }
+})
